2667e5b3810c111cfe8af8ccf5959853006041f4
Josh Berumen <joshberumen@Joshs-MacBook-Pro.local>
2024-05-01 13:24:26 -0700

    adding allowed origins for server actions

--- next.config.js
+++ next.config.js
@@ -6,4 +6,12 @@ module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
     },
+    experimental: {
+        serverActions: {
+            allowedOrigins: [
+                "localhost:3000",
+                "project-tracker.jdbcoder.com:443",
+            ]
+        }
+    }
 };
